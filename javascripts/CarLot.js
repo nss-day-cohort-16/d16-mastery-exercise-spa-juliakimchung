34967c7b52422e75d62faccc4770b87551d36340
juliakimchung <julhkm@gmail.com>
2016-11-15 09:35:21 -0600

    made changes on the input box, keypress function, deleted unnecessary console.log, fixed indentation

--- javascripts/CarLot.js
+++ javascripts/CarLot.js
@@ -2,33 +2,25 @@
 
 var CarLot = (function (oldCarLot) {
 
-    var privateInventory = {};
+  var privateInventory = {};
 
-   
-    // oldCarLot.getInventory = function () { 
-    //   //console.log("loadInventory function loads" );
-    //   return privateInventory;
+  oldCarLot.loadInventory = function () { 
 
-    // };
-
-    oldCarLot.loadInventory = function () { 
-  
     var inventoryLoader = new XMLHttpRequest();
 
-      inventoryLoader.open("GET", "javascripts/inventory.json");
-      inventoryLoader.send();
+    inventoryLoader.open("GET", "javascripts/inventory.json");
+    inventoryLoader.send();
 
-      inventoryLoader.addEventListener("load", function () {
+    inventoryLoader.addEventListener("load", function () {
       privateInventory = JSON.parse(this.responseText).cars;
-        
-        //console.log("privateInventory", privateInventory );
-        //pass data to quiz.js
-        CarLot.populatePage(privateInventory);
-       });
+      
+      
+      CarLot.populatePage(privateInventory);
+    });
 
-      };
+  };
 
-      return oldCarLot;
+  return oldCarLot;
     
     
 })(CarLot|| {});
--- javascripts/event.js
+++ javascripts/event.js
@@ -1,64 +1,59 @@
+
 "use strict";
-  
-var CarLot = (function(oldCarLot) {
+
+ var CarLot = (function(oldCarLot) {
 	
 
 	oldCarLot.activateEvents = function() {
-	
+
 	var allCards = document.querySelectorAll(".carCard");
-  	var inputText = document.getElementById("inputText");
-	var selectedCard;
+	var inputText = document.getElementById("inputText");
+	var submit = document.getElementById("submitBtn");
+    var selectedCard = event.currentTarget;
 
-	function selectACard (event) {
+		$("#inputText").keypress (function editCard(event){
+		    //if(event.keyCode === 13){
+			//if(!selectedCard){alert("please choose your car")}
+			var edition = selectedCard.querySelector(".description");
+			
+		    edition.innerHTML = inputText.value;
+			}
+			
+		);
+			for (var i = 0; i < allCards.length; i++) {
+				selectedCard = allCards[i];
+			}
+		function selectACard (){
 
-	inputText.value = " ";
-	inputText.focus();
-	selectedCard = event.currentTarget;
-	CarLot.removeCard();
-	CarLot.addSelectedCard("selectedCard");
+		inputText.value = " ";
+		inputText.focus();
+		selectedCard = event.currentTarget;
+		CarLot.removeCard();
+		CarLot.addSelectedCard("selectedCard");
+		}
+			for (var i = 0; i < allCards.length; i++) { 
+			var currentCard = allCards[i];
+			currentCard.addEventListener("click", selectACard);
+			}
 
 
-  }
-	for (var i = 0; i < allCards.length; i++) { 
-		var currentCard = allCards[i];
-		console.log("allCards[i]", allCards[i]);
-	currentCard.addEventListener("click", selectACard);
+		oldCarLot.removeCard = function() {
+			var allCards = document.querySelectorAll(".carCard");
+			for (var i = 0; i < allCards.length; i++) { 
+			var currentCard = allCards[i];
+					
+			currentCard.classList.remove("selectedCard");
 
-}
-	console.log(allCards );
+			}
+		};
 
-
-  	inputText.addEventListener("keypress", function editCard(event) {
-
-  	var edition = selectedCard.querySelector(".description");
- 	 edition.innerHTML = inputText.value;
-
-});
-
-	var selectedCard = event.currentTarget;
-
-	oldCarLot.removeCard = function() {
-	var allCards = document.querySelectorAll(".carCard");
-	for (var i = 0; i < allCards.length; i++) { 
-		var currentCard = allCards[i];
-		
-	currentCard.classList.remove("selectedCard");
-
-}
-};
-
-	oldCarLot.addSelectedCard = function() {
-		selectedCard.classList.add("selectedCard");
+		oldCarLot.addSelectedCard = function() {
+			selectedCard.classList.add("selectedCard");
+		};
 	};
-
-  //var submit = document.querySelector(".submitBtn");	
-
-
-
-}
 	
 
- return CarLot;
+ 	return CarLot;
 
 
 	
